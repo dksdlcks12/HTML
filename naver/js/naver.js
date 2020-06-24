533e8d22950bf0bc9c07fc20237e8aea90a0b266
AJS <dksdlcks12@naver.com>
2020-06-24 17:49:39 +0900

    day45

--- naver/js/naver.js
+++ naver/js/naver.js
@@ -1,5 +1,9 @@
 $(function(){
-	$('html').scrollTop(0);
+	window.onload = function(){
+		setTimeout(function(){
+			scrollTo(0,0)
+		}, 100)
+	}
 	// 설정한 메뉴 갯수
 	var checkcont
 	// 메뉴 세이브포인트
@@ -9,7 +13,8 @@ $(function(){
 	// 리셋포인트
 	var resetpoint = $('.changeable-menu').html()
 	// 메뉴 설정시 moremenu 화면 선택하는 함수
-	$.menuselect = function(){
+	$('a').click(function(e){e.preventDefault()})
+	$menuselect = function(){
 		$('.moremenu-setting .moremenu-item').click(function(e){
 		var i = 0
 		checkcont = $('.check-icon-checked').length
@@ -51,7 +56,7 @@ $(function(){
 		e.preventDefault()
 	})}	
 	// 초기화 함수
-	$.reset = function(){
+	$reset = function(e){
 		savemenu = null
 		savecheck = null
 		$('.changeable-menu').html(resetpoint)
@@ -68,7 +73,15 @@ $(function(){
 		$('.save').css('display', 'none')
 		$('.moremenu-setting .moremenu-item').removeClass('check-icon-checked')
 		$('.changeable-menu-empty').empty()
+		e.preventDefault()
 	}
+	var newsisuelist = []
+	var newsisuelistindex = 0
+	newsisuelist.push('55→61→92 부산항 확진 러 선원 밀접접촉자 계속 늘어')
+	newsisuelist.push('분당 유주택자가 잠실 집 사면 원래 집 처분계획서 내야')
+	newsisuelist.push("재난지원금 다 썼나…골목상권 '반짝 특수' 뒤 원위치")
+	var lollnewshover = 0
+	var newspage = 0
 	// 상단배너 닫기
 	$('.bt-close').click(function(e){
 		$('.box-whale').css('display', 'none')
@@ -100,7 +113,7 @@ $(function(){
 		e.preventDefault()
 	})
 	// 실시간 검색어창 열기
-	$('.link-keyword').click(function(e){
+	$('.keyword-window').click(function(e){
 		$('.rtk-view').css('display', 'block')
 		e.preventDefault()
 	})
@@ -120,7 +133,7 @@ $(function(){
 		e.preventDefault()
 	})
 	// 메뉴설정시 moermenu를 선택 할수 있도록 하는 함수
-	$.menuselect()
+	$menuselect()
 	// 더보기 버튼 클릭시
 	$('.more-bt').click(function(e){
 		if($(this).hasClass('more-bt-on')){
@@ -157,7 +170,7 @@ $(function(){
 		if(!(savecheck==null)){
 			$('.moremenu-setting').html(savecheck)
 			$('.changeable-menu-empty').html(savemenu)
-			$.menuselect()
+			$menuselect()
 		}
 		checkcont = $('.check-icon-checked').length
 		for(i=0;i<(5-checkcont);++i){
@@ -173,7 +186,7 @@ $(function(){
 	$('.save').click(function(e){
 		if(checkcont==0){
 			alert('선택된 메뉴가 없습니다. 초기설정으로 돌아갑니다.')
-			$.reset()
+			$reset()
 		}else{
 			savemenu = ''
 			savecheck = $('.moremenu-setting').html()
@@ -201,6 +214,115 @@ $(function(){
 	// 초기화 버튼 클릭시
 	$('.reset').click(function(e){
 		alert('초기설정으로 돌아갑니다.')
-		$.reset()
+		$reset()
 	})
+	$('.main-news-isuetitle').hover(function(){
+		lollnewshover=1
+	})
+	$('.main-news-isuetitle').mouseleave(function(){
+		lollnewshover=0
+	})
+	setInterval(function(){
+		if(lollnewshover==1){
+			$('.main-news-isuetitlebox').animate({'margin-top':'0px'},500,function(){
+			$(this).css('margin-top', '0px')
+		})
+		}
+		if(lollnewshover==0){
+			$('.main-news-isuetitlebox').append('<div class="main-news-isuetitleview"><a href="//news.naver.com/main/list.nhn?mode=LPOD&mid=sec&sid1=001&sid2=140&oid=001&isYeonhapFlash=Y&aid=0011695381" class="main-news-isuetitle">'+ newsisuelist[newsisuelistindex] +'</a></div>')
+			$('.main-news-isuetitlebox').animate({'margin-top':'-24px'},500,function(){
+				$(this).css('margin-top', '0px')
+				$('.main-news-isuetitlebox').children().eq(0).remove()
+			})
+			newsisuelistindex++
+			if(newsisuelistindex==newsisuelist.length){
+				newsisuelistindex=0
+			}
+		}
+		$('.main-news-isuetitle').hover(function(){
+			lollnewshover=1
+		})
+		$('.main-news-isuetitle').mouseleave(function(){
+			lollnewshover=0
+		})
+	},3500)
+	$('.main-news-viewsortbt').click(function(e){
+		$('.main-news-viewsortbt').removeClass('main-news-viewsorton')
+		$(this).addClass('main-news-viewsorton')
+		e.preventDefault()
+	})
+	$('.main-news-setbt').click(function(e){
+		$('.main-news-setbt').removeClass('main-news-seton')
+		$(this).addClass('main-news-seton')
+		e.preventDefault()
+	})
+	$('.main-news-subscribe').click(function(e){
+		$(this).next().css('display','inline-block')
+		$(this).css('display','none')
+		e.preventDefault()
+	})
+	$('.main-news-unsubscribe').click(function(e){
+		$(this).prev().css('display','inline-block')
+		$(this).css('display','none')
+		e.preventDefault()
+	})
+	$('.main-news-logobox').hover(function(e){
+		$('.main-news-logopopup').css('display','none')
+		$(this).siblings('.main-news-logopopup').css('display','block')
+		e.preventDefault()
+	})
+	$('.main-news-logopopup').mouseleave(function(e){
+		$('.main-news-logopopup').css('display','none')
+		e.preventDefault()
+	})
+	$('.main-news-prevbt').css('display', 'none')
+	$('.main-news-window').css('display', 'none')
+	$('.main-news-window').eq(newspage).css('display', 'block')
+	$('.main-news-prevbt').click(function(e){
+		if(0<newspage){
+			--newspage
+			$('.main-news-window').css('display', 'none')
+			$('.main-news-window').eq(newspage).css('display', 'block')
+		}
+		if(newspage==0){
+			$('.main-news-prevbt').css('display', 'none')
+		}else{
+			$('.main-news-prevbt').css('display', 'block')
+		}
+		if(newspage==($('.main-news-window').length-1)){
+			$('.main-news-nextbt').css('display', 'none')
+		}else{
+			$('.main-news-nextbt').css('display', 'block')
+		}
+		e.preventDefault()
+	})
+	$('.main-news-nextbt').click(function(e){
+		if(newspage<($('.main-news-window').length-1)){
+			++newspage
+			$('.main-news-window').css('display', 'none')
+			$('.main-news-window').eq(newspage).css('display', 'block')
+		}
+		if(newspage==0){
+			$('.main-news-prevbt').css('display', 'none')
+		}else{
+			$('.main-news-prevbt').css('display', 'block')
+		}
+		if(newspage==($('.main-news-window').length-1)){
+			$('.main-news-nextbt').css('display', 'none')
+		}else{
+			$('.main-news-nextbt').css('display', 'block')
+		}
+		e.preventDefault()
+	})
+	setInterval(function(){
+		if(newspage<($('.main-news-window').length-1)){
+			++newspage
+			$('.main-news-window').css('display', 'none')
+			$('.main-news-window').eq(newspage).css('display', 'block')
+			$('.main-news-prevbt').css('display', 'block')
+		}
+		if(newspage==($('.main-news-window').length-1)){
+			$('.main-news-nextbt').css('display', 'none')
+		}
+	}, 15000)
 })
